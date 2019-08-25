40684665f1a3a5d762c1d32939e84b16923c10a6
Fatih Bozik <bozikfatih@gmail.com>
2019-08-25 23:17:56 +0300

    Update .gitignore file

--- methods-and-encapsulation/src/main/java/io/github/fatihbozik/oca/example6/pond/inland/BirdWatcherFromAfar.java
+++ methods-and-encapsulation/src/main/java/io/github/fatihbozik/oca/example6/pond/inland/BirdWatcherFromAfar.java
@@ -1,8 +1,10 @@
 package io.github.fatihbozik.oca.example6.pond.inland;//package pond.inland;
 
-import io.github.fatihbozik.oca.ch04.example6.pond.shore.Bird;
 
-public class BirdWatcherFromAfar {
+import io.github.fatihbozik.oca.example6.pond.goose.Gosling;
+import io.github.fatihbozik.oca.example6.pond.shore.Bird;
+
+public class BirdWatcherFromAfar extends Gosling {
 
     public void watchBird() {
 	Bird bird = new Bird();
